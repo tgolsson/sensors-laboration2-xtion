d866d2d78f7780441856374bbe486907e87ede9f
Tom Olsson <mail@tomolsson.se>
2015-11-24 12:40:57 +0100

    Adding template for report.

---
+++ report/auto/Report.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "Report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "preamble"
+    "article"
+    "art11")
+   (LaTeX-add-bibliographies
+    "References")))
+
---
+++ report/auto/preamble.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "preamble"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "a4paper" "top=1in" "bottom=1.1in" "left=1in" "right=1in") ("inputenc" "utf8") ("fontenc" "T1")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "graphicx"
+    "caption"
+    "geometry"
+    "inputenc"
+    "fontenc"
+    "xcolor"
+    "listings"
+    "subcaption"
+    "siunitx"
+    "wrapfig"
+    "varioref"
+    "amsmath"
+    "commath"
+    "mathtools"
+    "url")
+   (TeX-add-symbols
+    '("di" 2))
+   (LaTeX-add-caption-DeclareCaptions
+    '("\\DeclareCaptionFormat{myformat}" "Format" "myformat"))))
+
